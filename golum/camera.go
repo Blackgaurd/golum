f73e3f2eaa14891175c682f5558ae91e1d2a47df
Bryan Deng <bryan.deng002@gmail.com>
2023-12-24 00:23:18 -0500

    add camera

---
+++ golum/camera.go
@@ -0,0 +1,36 @@
+package golum
+
+import (
+	"math"
+	"math/rand"
+)
+
+type Camera struct {
+	pos, forward, up, right Vec3
+	fovx                    float64
+}
+
+func LookAt(pos, look_at, up Vec3, fovx float64) Camera {
+	forward := look_at.SubVec(pos)
+	return Camera{
+		pos,
+		forward.Normalize(),
+		up.Normalize(),
+		forward.Cross(up).Normalize(),
+		fovx * math.Pi / 180,
+	}
+}
+
+func (cam Camera) GetRay(w, h, x, y int, rng *rand.Rand) (Vec3, Vec3) {
+	forward_dis := float64(w) / math.Tan(cam.fovx/2)
+	right_dis := float64(x) - float64(w)/2 + rng.Float64()
+	up_dis := float64(y) - float64(h)/2 + rng.Float64()
+
+	ray_d := Vec3{}
+	ray_d = ray_d.AddVec(cam.forward.MulScalar(forward_dis))
+	ray_d = ray_d.AddVec(cam.right.MulScalar(right_dis))
+	ray_d = ray_d.AddVec(cam.up.MulScalar(up_dis))
+	ray_d = ray_d.Normalize()
+
+	return ray_d, cam.pos
+}
